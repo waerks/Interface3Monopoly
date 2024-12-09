8df2b0fa40909dea7ce5893991fdd8e639f83877
Anaïs <a.aerts@interface3.be>
2024-12-09 10:14:30 +0100

    Push EX03

---
+++ EX03/Models/CasePropriete.cs
@@ -0,0 +1,36 @@
+﻿using EX03.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EX03.Models
+{
+	public class CasePropriete
+	{
+		/* Version complète (seulement si vérification en entrée et sortie)
+        private string _nom;
+        public string Nom
+        {
+            get { return _nom; }
+            private set { _nom = value; }
+        }*/
+		/*Auto-proprétée (Seulement si aucune vérification)*/
+		public string Nom { get; }
+		public Couleurs Couleur { get; }
+		public int Prix { get; }
+		public bool EstHypotequee { get; }
+		public Joueur Proprietaire { get; }
+		public CasePropriete(string nom, Couleurs couleur, int prix)
+		{
+			Nom = nom;
+			Couleur = couleur;
+			Prix = prix;
+			/* Pas nécessaire de les affecter, les variables auront les bonnes valeurs car valeur par défaut de leur type
+            
+            EstHypotequee = false;
+            Proprietaire = null;
+            */
+		}
+	}
+
---
+++ EX03/Models/Joueur.cs
@@ -0,0 +1,34 @@
+﻿using EX03.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX03.Models
+{
+	public class Joueur
+	{
+		private int _position;
+		public string Nom { get; set; }
+		public Pions Pion { get; set; }
+		public int Position
+		{
+			get
+			{
+				return _position;
+			}
+			private set
+			{
+				_position = value;
+			}
+		}
+		public int Solde { get; private set; }
+		public bool Avancer()
+		{
+			int[] result = De.Lancer(2);
+			Position += result[0] + result[1];
+			return result[0] == result[1];
+		}
+	}
+}
