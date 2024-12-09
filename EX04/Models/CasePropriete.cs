c613303672a2ef802f5d57504821562ee2d89d89
Anaïs <a.aerts@interface3.be>
2024-12-09 10:20:54 +0100

    Push EX04

---
+++ EX04/Models/CasePropriete.cs
@@ -0,0 +1,47 @@
+﻿using EX04.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EX04.Models
+{
+
+	public class CasePropriete
+	{
+		/* Version complète (seulement si vérification en entrée et sortie)
+        private string _nom;
+        public string Nom
+        {
+            get { return _nom; }
+            private set { _nom = value; }
+        }*/
+		/*Auto-proprétée (Seulement si aucune vérification)*/
+		public string Nom { get; }
+		public Couleurs Couleur { get; }
+		public int Prix { get; }
+		public bool EstHypotequee { get; }
+		public Joueur Proprietaire { get; private set; }
+		public CasePropriete(string nom, Couleurs couleur, int prix)
+		{
+			Nom = nom;
+			Couleur = couleur;
+			Prix = prix;
+			/* Pas nécessaire de les affecter, les variables auront les bonnes valeurs car valeur par défaut de leur type
+            
+            EstHypotequee = false;
+            Proprietaire = null;
+            */
+		}
+
+		public void Acheter(Joueur acheteur)
+		{
+			if (acheteur is null) return;           //Gérer avec une Exception
+			if (Proprietaire == acheteur) return;   //Gérer avec une Exception
+			if (acheteur.Solde < Prix) return;      //Gérer avec une Exception
+			acheteur.Payer(Prix);
+			Proprietaire = acheteur;
+			acheteur.AjouterPropriete(this);
+		}
+	}
+}
---
+++ EX04/Program.cs
@@ -0,0 +1,74 @@
+﻿using EX04.Enums;
+using EX04.Models;
+
+namespace EX04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* Test class De
+            int[] result = De.Lancer(2);
+            Console.WriteLine( $"Premier dé : {result[0]}\nSecond dé : {result[1]}");
+            */
+
+			/* Test class Joueur */
+			Joueur j1 = new Joueur("Samuel", Pions.Dino);
+			Console.WriteLine($"{j1.Nom} c'est votre tour! Bougez le pion {j1.Pion} de la case {j1.Position}!");
+
+			if (j1.Avancer())
+			{
+				Console.WriteLine($"Bravo {j1.Nom}! Vous avez fait un double!");
+			}
+
+			Console.WriteLine($"{j1.Nom} vous êtes à présent sur la case {j1.Position}!");
+
+			Console.WriteLine($"Votre solde est de {j1.Solde} $Monopoly.");
+
+			Console.WriteLine("Vos propriétés :");
+			foreach (CasePropriete prop in j1.Proprietes)
+			{
+				Console.WriteLine($"\t- {prop.Nom} ({prop.Couleur})");
+			}
+
+			/* Test de la class CasePropriete */
+			CasePropriete i3Patio = new CasePropriete("Patio Interface 3", Couleurs.Marron, 20);
+
+			Console.WriteLine($"La première case du jeu Monopoly Version I3 est :");
+
+			Console.WriteLine(i3Patio.Nom);
+
+			Console.WriteLine($"De couleur {i3Patio.Couleur}");
+
+			Console.WriteLine($"Coutant {i3Patio.Prix} $Monopoly");
+
+			if (i3Patio.EstHypotequee) Console.WriteLine("Ce terrain est hypotèqué...");
+			else Console.WriteLine("Ce terrain n'est pas hypotèqué.");
+			if (i3Patio.Proprietaire is null) Console.WriteLine("Ce terrain est en vente!");
+			else Console.WriteLine($"Ce terrain appartient à {i3Patio.Proprietaire.Nom}");
+
+			i3Patio.Acheter(j1);
+
+			Console.WriteLine($"La première case du jeu Monopoly Version I3 est :");
+
+			Console.WriteLine(i3Patio.Nom);
+
+			Console.WriteLine($"De couleur {i3Patio.Couleur}");
+
+			Console.WriteLine($"Coutant {i3Patio.Prix} $Monopoly");
+
+			if (i3Patio.EstHypotequee) Console.WriteLine("Ce terrain est hypotèqué...");
+			else Console.WriteLine("Ce terrain n'est pas hypotèqué.");
+			if (i3Patio.Proprietaire is null) Console.WriteLine("Ce terrain est en vente!");
+			else Console.WriteLine($"Ce terrain appartient à {i3Patio.Proprietaire.Nom}");
+			Console.WriteLine($"Votre solde est de {j1.Solde} $Monopoly.");
+
+			Console.WriteLine("Vos propriétés :");
+
+			foreach (CasePropriete prop in j1.Proprietes)
+			{
+				Console.WriteLine($"\t- {prop.Nom} ({prop.Couleur})");
+			}
+		}
+	}
+}
